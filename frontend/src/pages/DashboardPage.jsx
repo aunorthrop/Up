03b37a112fa6a6facd5274b6dfde39cee0103c61
aunorthrop <austinrnorthrop@icloud.com>
2025-09-16 21:59:59 -0400

    Update DashboardPage.jsx

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import { useAuth } from '../hooks/useAuth';
+import Dashboard from '../components/Dashboard/Dashboard';
+import Header from '../components/Common/Header';
+import Sidebar from '../components/Common/Sidebar';
 
+const DashboardPage = () => {
+  const { user } = useAuth();
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 p-6">
+          <Dashboard />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
