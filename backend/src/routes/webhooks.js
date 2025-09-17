e1059fd43174944404125f55203c15c847563eae
aunorthrop <austinrnorthrop@icloud.com>
2025-09-16 22:01:23 -0400

    Update webhooks.js

--- backend/src/routes/webhooks.js
+++ backend/src/routes/webhooks.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const webhookController = require('../controllers/webhookController');
+const router = express.Router();
 
+// Square webhooks
+router.post('/square', webhookController.handleSquareWebhook);
+
+// Shopify webhooks  
+router.post('/shopify', webhookController.handleShopifyWebhook);
+
+module.exports = router;
