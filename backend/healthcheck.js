dab0f2dd892ac1546691a6339f9dbddbc1b09953
aunorthrop <austinrnorthrop@icloud.com>
2025-09-16 22:09:37 -0400

    Create healthcheck.js

---
+++ backend/healthcheck.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const options = {
+  hostname: 'localhost',
+  port: process.env.PORT || 3001,
+  path: '/health',
+  timeout: 2000,
+};
+
+const request = http.request(options, (response) => {
+  console.log(`STATUS: ${response.statusCode}`);
+  if (response.statusCode === 200) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
+});
+
+request.on('error', (error) => {
+  console.log('ERROR:', error.message);
+  process.exit(1);
+});
+
+request.end();
